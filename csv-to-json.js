e9310909f5a07076872312518fe6e8f8bb1b3af6
Judith <judithrea98@gmail.com>
2020-03-24 12:34:37 +0100

    Initial commit

---
+++ csv-to-json.js
@@ -0,0 +1,45 @@
+// Importing modules: fs, path, csvtojson
+const fs = require('fs');
+const path = require('path');
+const csv = require('csvtojson');
+const csvFilePath = 'customer-data.csv';
+
+// Use fs module to read the csv file and extract the data
+fs.readFile(csvFilePath, 'utf-8', (err, data) => {
+
+  if (err) 
+    return console.error(err);
+
+  let lines = data.split('\r\n');
+  const keys = lines.shift().split(',');
+  const objs = [];
+
+  // Iterate through each line of the lines array (more specifically, each value) and create an array of objects 
+  for(let line of lines) {
+
+    // The remaining lines (after removing the keys from the csv file) are going to be the values of each key-value pair
+    // Lines are split into csv Fields, that is, the individual values that make up each piece of information about the customer
+    const values = line.split(',');
+    const obj = {};
+
+    // Iterate through the 'keys' array and assign each key to its value
+    keys.forEach((key, i) => {
+      obj[key] = values[i];
+    })
+
+    objs.push(obj);
+  }
+  // Delete last object, which doesn't represent any useful data
+  objs.pop();
+
+  // Alternative: use module csvtojson
+  /* csv()
+  .fromFile(csvFilePath)
+  .then((jsonObj) => {
+    console.log(jsonObj);
+  }) */
+
+  // Create a json from the objs array and save it in a new file 
+  fs.writeFileSync('customer-data.json', JSON.stringify(objs, null, '\t'))
+})
+
